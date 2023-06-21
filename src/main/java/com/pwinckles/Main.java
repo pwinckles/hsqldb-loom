d9cdbdaa9a6457abd2c900d21c32f86b6543da9b
Peter Winckles <pwinckles@pm.me>
2023-06-21 11:02:24 -0500

    test with tcp conns

--- src/main/java/com/pwinckles/Main.java
+++ src/main/java/com/pwinckles/Main.java
@@ -1,11 +1,15 @@
 package com.pwinckles;
 
+import java.beans.PropertyVetoException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.hsqldb.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,11 +18,19 @@ public final class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     private static final List<String> MODES = List.of("LOCKS", "MVLOCKS", "MVCC");
+    private static volatile ComboPooledDataSource c3p0;
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws Exception {
         if (args.length != 1 || !MODES.contains(args[0].toUpperCase())) {
             throw new IllegalArgumentException("Must specify transaction control mode: " + MODES);
         }
+
+        var server = new Server();
+        server.setDatabaseName(0, "testdb");
+        server.setDatabasePath(0, "mem:testdb");
+        server.start();
+
+        initC3P0Pool();
 
         try (var conn = newConnection()) {
             setupDb(args[0]);
@@ -41,6 +53,8 @@ public final class Main {
                     });
                 }
             }
+        } finally {
+            server.stop();
         }
     }
 
@@ -53,6 +67,16 @@ public final class Main {
     }
 
     private static Connection newConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:hsqldb:mem:testdb", "SA", "");
+        return DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/testdb", "SA", "");
+//        return c3p0.getConnection();
     }
+
+    private static void initC3P0Pool() throws PropertyVetoException {
+        c3p0 = new ComboPooledDataSource();
+        c3p0.setDriverClass("org.hsqldb.jdbcDriver");
+        c3p0.setJdbcUrl("jdbc:hsqldb:hsql://localhost/testdb");
+        c3p0.setUser("SA");
+        c3p0.setPassword("");
+    }
+
 }
